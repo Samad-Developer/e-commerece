79021bacbd60f4df00d5f2f7ced3623be1e40119
Abdus samad <abdussamadkotha@gmail.com>
2024-03-11 18:16:50 +0500

    home page and category added

---
+++ app/[category]/page.tsx
@@ -0,0 +1,68 @@
+
+'use client'
+import { useParams } from 'next/navigation'
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaShoppingCart } from 'react-icons/fa';
+ 
+const Page = () => {
+  interface Product {
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+    category: string;
+    description: string;
+  }
+  const params = useParams<{ category: string }>()
+  const category = params.category;
+  const [products, setProducts] = useState<Product[]>([]);
+  console.log(products)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <main className="mt-10 flex flex-col items-center justify-center min-h-screen p-6">
+      <div className="grid grid-cols-4 gap-4 font-inter">
+        {products.map((product) => (
+          <div key={product.id} className="shadow-xl bg-[#FFFFFF] w-[302px] h-[520px] p-4">
+            <div className="w-40 h-40 relative rounded-md overflow-hidden">
+              <Image
+                src={product.image}
+                alt={product.title}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <div className=" flex flex-col justify-between">
+              <div>
+                <h2 className="text-xl font-semibold line-clamp-1 mt-8 mb-1">{product.title}</h2>
+                <p className="text-gray-600 mb-6">{product.category}</p>
+                <p className="text-gray-600 mb-6 line-clamp-3">{product.description}</p>
+              </div>
+              <p className="text-gray-600 mt-2 mb-6">price <span className='font-bold'>${product.price}</span></p>
+              <button
+      className="flex justify-center px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+    >
+      Add to Cart
+      <FaShoppingCart className="ml-2" />
+    </button>            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,51 @@
+'use client'
+import { useState, useEffect, Suspense } from "react";
 import Image from "next/image";
+import Link from "next/link";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data: Product[] = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   
+    <main className="mt-24 flex flex-col items-center justify-center min-h-screen p-6">
+      <div className="grid grid-cols-3 gap-8">
+        {products.map((product) => (
+          <div key={product.id} className="border rounded-md p-4">
+            <Image
+              src={product.image}
+              alt={product.title}
+              width={200}
+              height={200}
+            />
+            <h2 className="text-xl font-semibold mt-2">{product.title}</h2>
+            <p className="text-gray-600">{product.price}</p>
+          </div>
+        ))}
+      </div>
+      <Link href="/dashboard?search=my-project">Click me</Link>
     </main>
+   
   );
 }
