359711408e357d2818b427110680a82debaf0b75
Abdus samad <abdussamadkotha@gmail.com>
2024-04-07 10:57:26 +0500

    managing navbar and installing shadcn ui

---
+++ app/components/ui/Navbar.tsx
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import Image from 'next/image'
+import { BiSearch } from 'react-icons/bi';
+import { FaShoppingCart } from 'react-icons/fa';
+import { FaEllipsisV } from 'react-icons/fa';
+import image from '../../../public/next.svg'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+
+interface Category {
+    name: string;
+    path: string;
+}
+
+const Navbar = () => {
+    const categories: Category[] = [
+        { name: "electronics", path: "/electronics" },
+        { name: "men's clothing", path: "/men's clothing" },
+        { name: "women's clothing", path: "/women's clothing" },
+        { name: "jewelery", path: "/jewelery" },
+    ];
+    return (
+        <nav className="w-full h-16 md:h-20 lg:h-24 flex flex-wrap items-center justify-between bg-[#FFFCFA] px-8">
+            {/* Logo or Brand */}
+            <Image src={image} alt="Logo" width={100} height={100} />
+            {/* Navigation Links */}
+            <ul className={`hidden myown:flex items-center space-x-8 mt-4 md:mt-0 w-full md:w-auto`}>
+                {categories.map((category, index) => (
+                    <li key={index} className="text-sm md:text-base lg:text-lg">
+                        <Link href={`${category.path}`}>
+                            {category.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            {/* Search Box */}
+            <div className="hidden myown:flex items-center mt-4 md:mt-0">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="px-4 py-2 rounded-l-md focus:outline-none"
+                />
+                <button className="px-4 py-2 text-[25px] rounded-r-md focus:outline-none">
+                    <BiSearch />
+                </button>
+            </div>
+            {/* Cart */}
+            <div className="hidden myown:flex items-center mt-4 md:mt-0">
+                <FaShoppingCart className="h-8 w-8 text-gray-600" />
+            </div>
+            {/* mobile menu */}
+            <Popover>
+                <PopoverTrigger><FaEllipsisV className="myown:hidden h-6 w-6 text-gray-600" /></PopoverTrigger>
+                <PopoverContent>
+                    <div className="flex justify-end"><FaShoppingCart className="h-8 w-8 text-gray-600" /></div>
+                    {/* menu links */}
+                    <ul className={`flex-col items-center mt-4`}>
+                        {categories.map((category, index) => (
+                            <li key={index} className="text-sm md:text-base lg:text-lg">
+                                <Link href={`${category.path}`}>
+                                    {category.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    {/* search box */}
+                    <div className="flex mt-4">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className=" rounded-l-md focus:outline-none"
+                        />
+                        <button className=" rounded-r-md focus:outline-none">
+                            <BiSearch />
+                        </button>
+                    </div>
+                </PopoverContent>
+            </Popover>
+
+        </nav>
+    );
+}
+
+export default Navbar;
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,31 @@
 export default function Loading() {
-    return <div>Loading...</div>;
+    return   <div className="grid grid-cols-4 gap-4 p-20">
+      {/* First Row */}
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+
+      {/* Second Row */}
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+      <div className="animate-pulse bg-gray-300 rounded-lg p-4 p-20">
+        {/* Content of the card */}
+      </div>
+    </div>;
   }
